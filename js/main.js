20d625e0928a2911d3f1d04da0677c3ac74b0795
babaev <sergeybabaev063@mail.ru>
2017-06-01 16:23:16 +0300

    add button read more

--- js/main.js
+++ js/main.js
@@ -2,31 +2,55 @@ const CONTACTS = [{
     id: 1,
     name: 'Rodion Adamenko',
     phoneNumber: '+380639999999',
-    image: 'images/bender.jpg'
+    image: 'images/bender.jpg',
+    description: 'I want to tell you little beat more...'
 },{
     id: 2,
     name: 'Evgeniy Gladkih',
     phoneNumber: '+380633333333',
-    image: 'images/fray.jpg'
+    image: 'images/fray.jpg',
+    description: 'I want to tell you little beat more...'
 },{
     id: 3,
     name: 'Nikolay White',
     phoneNumber: '+380632222222',
-    image: 'images/zoidberg.jpg'
+    image: 'images/zoidberg.jpg',
+    description: 'I want to tell you little beat more...'
 },{
     id: 4,
     name: 'Elena Pira',
     phoneNumber: '+380632222222',
-    image: 'images/lila.jpg'
+    image: 'images/lila.jpg',
+    description: 'I want to tell you little beat more...'
 }];
 
 let Contact = React.createClass({
+
+    getInitialState: function() {
+        return {
+            visible: false
+        }
+    },
+    showMore: function(e) {
+        let more = e.target.parentNode.getElementsByClassName('more')[0];
+        this.setState({
+            visible: !this.state.visible
+        });
+        if (this.state.visible) {
+            more.style.display = 'block';
+        } else {
+            more.style.display = 'none';
+        }
+    }
+    ,
     render: function() {
         return <li className="contact">
                     <img  className="contact-image" src={this.props.image} width="60px" height="60px"/>
                     <div className="contact-info">
                         <div className="contact-name">{this.props.name}</div>
                         <div className="contact-number">{this.props.phoneNumber}</div>
+                        <a href="#" className="read-more" onClick={this.showMore}>Подробнее</a>
+                        <div className="more">{this.props.description}</div>
                     </div>
                 </li>
     }
@@ -47,7 +71,6 @@ let ContactList = React.createClass({
         this.setState({
             displayedContacts: displayedContacts
         });
-        console.log(displayedContacts);
     },
     render: function() {
         return (
@@ -61,7 +84,7 @@ let ContactList = React.createClass({
                                 name={el.name}
                                 phoneNumber = {el.phoneNumber}
                                 image = {el.image}
-
+                                description={el.description}
                         />})
                     }
                 </ul>
